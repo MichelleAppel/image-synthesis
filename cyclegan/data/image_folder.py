79ca834ff91ee96a3dfb73d70eb29c3cdff63151
appel.michelle@gmail.com <appel.michelle@gmail.com>
2022-12-01 15:56:27 +0100

    dataset

--- cyclegan/data/image_folder.py
+++ cyclegan/data/image_folder.py
@@ -20,15 +20,21 @@ def is_image_file(filename):
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
 
 
-def make_dataset(dir, max_dataset_size=float("inf")):
+def make_dataset(dir, max_dataset_size=float("inf"), req_in_name=None):
     images = []
     assert os.path.isdir(dir), '%s is not a valid directory' % dir
 
     for root, _, fnames in sorted(os.walk(dir)):
         for fname in fnames:
             if is_image_file(fname):
-                path = os.path.join(root, fname)
-                images.append(path)
+                if req_in_name != None:
+                    if req_in_name in fname:
+                        path = os.path.join(root, fname)
+                        images.append(path)
+                else:
+                    path = os.path.join(root, fname)
+                    images.append(path)
+
     return images[:min(max_dataset_size, len(images))]
 
 
