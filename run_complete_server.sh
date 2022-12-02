871a22c77b5db3b5d3d850a46d9169d254b86b75
appel.michelle@gmail.com <appel.michelle@gmail.com>
2022-12-02 15:52:56 +0100

    args

--- run_complete_server.sh
+++ run_complete_server.sh
@@ -22,7 +22,7 @@ batch_size=32
 
 # python ./cyclegan/train.py --dataroot $root --use_wandb --name $run_name --batch_size 32 --load_size 448 --crop_size 448 --gpu_ids $1
 
-python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_fake --batch_size $batch_size --modality outlines --n_classes 2 --mode train --domain_transfer True --data_root $data_root_fake_train --net_G_path $net_G_path --gpu $1
+python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_fake --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_fake_train --gpu $1 # --domain_transfer True --net_G_path $net_G_path
 python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_fake --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_fake_test --load ./checkpoints/$run_name_fake/checkpoint_epoch5.pth --save_path ./output/$project_name_$run_name_fake/ --gpu $1 # --domain_transfer True --net_G_path $net_G_path
 python ./image_synthesis/test.py  --project_name $project_name --run_name $run_name_fake --n_images 64 --root $root$data_root_fake_test
 
