4f5f5abe9cf8694e26713e037b7dc5fcd0cee4a0
appel.michelle@gmail.com <appel.michelle@gmail.com>
2022-12-19 10:21:48 +0100

    run

--- run_complete.ps1
+++ run_complete.ps1
@@ -24,6 +24,6 @@ $batch_size = 1
 # python .\image_synthesis\train.py --project_name $project_name --run_name $run_name_fake --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_fake_test --load ./checkpoints/$run_name_fake/checkpoint_epoch5.pth --save_path .\output\$project_name_$run_name_fake\ # --domain_transfer True --net_G_path $net_G_path
 # python .\image_synthesis\test.py  --project_name $project_name --run_name $run_name_fake --n_images 64 --root .\output\$project_name_$run_name_fake\
 
-python .\image_synthesis\train.py --project_name $project_name --run_name $run_name_real --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_real_train
+python .\image_synthesis\train.py --project_name $project_name --run_name $run_name_real --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_real_train -s 0.05
 # python .\image_synthesis\train.py --project_name $project_name --run_name $run_name_real --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_fake_test --load ./checkpoints/$run_name_real/checkpoint_epoch5.pth --save_path .\output\$project_name_$run_name_real\ # --domain_transfer True --net_G_path $net_G_path
 # python .\image_synthesis\test.py  --project_name $project_name --run_name $run_name_real --n_images 64 --root .\output\$project_name_$run_name_real\
