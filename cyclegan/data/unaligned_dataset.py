9fe66b611ba12b9a371160cc999ce01232a41e1e
appel.michelle@gmail.com <appel.michelle@gmail.com>
2023-02-14 12:26:23 +0100

    yeahyeah

--- cyclegan/data/unaligned_dataset.py
+++ cyclegan/data/unaligned_dataset.py
@@ -28,7 +28,8 @@ class UnalignedDataset(BaseDataset):
         # self.dir_A = os.path.join(opt.dataroot, 'fake_labeled100k')
         self.dir_B = os.path.join(opt.dataroot, opt.phase + 'B')  # create a path '/path/to/data/trainB'
 
-        self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size, req_in_name='img'))  # load images from '/path/to/data/trainA'
+        # self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size, req_in_name='img'))  # load images from '/path/to/data/trainA'
+        self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size))         
         self.B_paths = sorted(make_dataset(self.dir_B, opt.max_dataset_size))    # load images from '/path/to/data/trainB'
         self.A_size = len(self.A_paths)  # get the size of dataset A
         self.B_size = len(self.B_paths)  # get the size of dataset B
@@ -50,7 +51,6 @@ class UnalignedDataset(BaseDataset):
             A_paths (str)    -- image paths
             B_paths (str)    -- image paths
         """
-        print(index, self.A_size)
         A_path = self.A_paths[index % self.A_size]  # make sure index is within then range
         if self.opt.serial_batches:   # make sure index is within then range
             index_B = index % self.B_size
