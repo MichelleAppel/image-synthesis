17fb6d285a1d2531cd1128deff14768f5ac3fe9e
appel.michelle@gmail.com <appel.michelle@gmail.com>
2023-02-21 14:01:28 +0100

    run

--- run_complete_server.sh
+++ run_complete_server.sh
@@ -30,24 +30,24 @@ steps=40000
 ## Train cycleGAN
 # python ./cyclegan/train.py --dataroot $root --use_wandb --name $cycleGANrun_name --batch_size 1 --load_size 448 --crop_size 256 --gpu_ids $1 --netG unet_256
 
-# Edge detection on NYUD dataset
-## Train
-python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_real --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_real_train --gpu $1 --steps $steps
+# # Edge detection on NYUD dataset
+# ## Train
+# python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_real --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_real_train --gpu $1 --steps $steps
 
-### Test
-python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_real --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_fake_test --load ./checkpoints/$project_name/$run_name_real/checkpoint_epoch30.pth --save_path ./output/$project_name/$run_name_real/fake/ --gpu $1 # --domain_transfer True --net_G_path $net_G_path
-python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_real --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_real_test --load ./checkpoints/$project_name/$run_name_real/checkpoint_epoch30.pth --save_path ./output/$project_name/$run_name_real/real/ --gpu $1
+# ### Test
+# python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_real --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_fake_test --load ./checkpoints/$project_name/$run_name_real/checkpoint_epoch30.pth --save_path ./output/$project_name/$run_name_real/fake/ --gpu $1 # --domain_transfer True --net_G_path $net_G_path
+# python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_real --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_real_test --load ./checkpoints/$project_name/$run_name_real/checkpoint_epoch30.pth --save_path ./output/$project_name/$run_name_real/real/ --gpu $1
 
 ## Evaluation
 # python ./image_synthesis/test.py  --project_name $project_name --run_name $run_name_real --n_images 100 --root $data_root_fake_test
 
-# # Edge detection on BDSD dataset
-# ## Train
-# python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_BDSD --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_BDSD_train --gpu $1 --steps $steps
+# Edge detection on BDSD dataset
+## Train
+python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_BDSD --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_BDSD_train --gpu $1 --steps $steps
 
-# # ### Test
-# python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_BDSD --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_fake_test --load ./checkpoints/$project_name/$run_name_real/checkpoint_epoch30.pth --save_path ./output/$project_name/$run_name_real/fake/ --gpu $1 # --domain_transfer True --net_G_path $net_G_path
-# python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_BDSD --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_BDSD_test --load ./checkpoints/$project_name/$run_name_real/checkpoint_epoch30.pth --save_path ./output/$project_name/$run_name_real/real/ --gpu $1
+# ### Test
+python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_BDSD --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_fake_test --load ./checkpoints/$project_name/$run_name_real/checkpoint_epoch30.pth --save_path ./output/$project_name/$run_name_real/fake/ --gpu $1 # --domain_transfer True --net_G_path $net_G_path
+python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_BDSD --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_BDSD_test --load ./checkpoints/$project_name/$run_name_real/checkpoint_epoch30.pth --save_path ./output/$project_name/$run_name_real/real/ --gpu $1
 
 ## Edge detection on fake dataset w/o domain transfer
 # # Train
