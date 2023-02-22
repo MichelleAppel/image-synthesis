11e84d64cb344c7359613d99e06b54bf9691b4a2
appel.michelle@gmail.com <appel.michelle@gmail.com>
2023-02-22 15:35:31 +0100

    run

--- run_complete_server.sh
+++ run_complete_server.sh
@@ -51,7 +51,7 @@ steps=40000
 
 ## Edge detection on fake dataset w/o domain transfer
 # # Train
-python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_fake_noDT --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_fake_train --gpu $1 --steps $steps
+# python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_fake_noDT --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_fake_train --gpu $1 --steps $steps
 
 # # Test
 # python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_fake_noDT --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_fake_test --load ./checkpoints/$project_name/$run_name_fake_noDT/checkpoint_step40000.pth --save_path ./output/$project_name/$run_name_fake_noDT/fake/ --gpu $1 # --domain_transfer True --net_G_path $net_G_path
@@ -63,7 +63,7 @@ python ./image_synthesis/train.py --project_name $project_name --run_name $run_n
 
 # # Edge detection on fake dataset with domain transfer
 # Train
-# python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_fake --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_fake_train --data_root_val $data_root_val --gpu $1 --domain_transfer True --net_G_path $net_G_path --steps $steps
+python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_fake --batch_size $batch_size --modality outlines --n_classes 2 --mode train --data_root $data_root_fake_train --data_root_val $data_root_val --gpu $1 --domain_transfer True --net_G_path $net_G_path --steps $steps
 
 # # Test
 # python ./image_synthesis/train.py --project_name $project_name --run_name $run_name_fake --batch_size $batch_size --modality outlines --n_classes 2 --mode test --data_root $data_root_fake_test --load ./checkpoints/$project_name/$run_name_fake/checkpoint_step40000.pth --save_path ./output/$project_name/$run_name_fake/fake/ --gpu $1 # --domain_transfer True --net_G_path $net_G_path
